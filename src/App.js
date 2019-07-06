ea4d91b984758f343d523e432501f56d872bdc8d
Steven <gonghaima@hotmail.com>
2019-07-06 14:13:26 +1000

    get data in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { data } from "./mock/productsData";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      clean up
-    </div>
-  );
+  return <div className="App">clean up - {JSON.stringify(data)}</div>;
 }
 
 export default App;
