b1beaff4aa488f57998ffe61b62dfff12a550e58
Steven <gonghaima@hotmail.com>
2019-07-07 20:01:43 +1000

    layout responsive

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import "./App.css";
 function App() {
   const headerStyle = { background: "lightgreen" };
   const footerStyle = { background: "lightgreen" };
+  const mainStyle = {
+    minHeight: "75vh",
+    padding: "5px"
+  };
   return (
     <div className="App">
       <div style={headerStyle}>header</div>
-      <div>main - {JSON.stringify(data.slice(0, 20))}</div>
+      <div style={mainStyle}>main - {JSON.stringify(data.slice(0, 20))}</div>
       <div style={footerStyle}>footer</div>
     </div>
   );
