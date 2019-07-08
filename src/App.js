ea47f95cad43b3146d927dd6efff61ed125dd89e
Steven <gonghaima@hotmail.com>
2019-07-08 19:05:11 +1000

    seperate to difference css module

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React from "react";
 import logo from "./logo.svg";
 import { data } from "./mock/productsData";
 import "./App.css";
+import styles from "./header.module.css";
+import footerStyles from "./footer.module.css";
+import mainStyles from "./main.module.css";
 
 function App() {
-  const headerStyle = { background: "lightgreen" };
-  const footerStyle = { background: "lightgreen" };
-  const mainStyle = {
-    minHeight: "75vh",
-    padding: "5px"
-  };
   return (
     <div className="App">
-      <div style={headerStyle}>header</div>
-      <div style={mainStyle}>main - {JSON.stringify(data.slice(0, 20))}</div>
-      <div style={footerStyle}>footer</div>
+      <div className={styles.general}>header</div>
+      <div className={mainStyles.general}>
+        main - {JSON.stringify(data.slice(0, 20))}
+      </div>
+      <div className={footerStyles.general}>footer</div>
     </div>
   );
 }
