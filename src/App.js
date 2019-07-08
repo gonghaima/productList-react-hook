989313228a23873137039ae53b82e3fe5483153c
Steven <gonghaima@hotmail.com>
2019-07-09 02:18:01 +1000

    add summary and select under title

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from "react";
 import logo from "./logo.svg";
 import { data } from "./mock/productsData";
 import "./App.css";
-import styles from "./header.module.css";
+import "./header.module.css";
+import headerStyles from "./header.module.css";
 import footerStyles from "./footer.module.css";
 import mainStyles from "./main.module.css";
 
 function App() {
   return (
     <div className="App">
-      <header className={styles.general}>
+      <header>
         <h2>All Products</h2>
+        <div className={headerStyles.displaySelection}>
+          <summary>1000 products</summary>
+          <select>
+            <option value="4">4 per page</option>
+            <option value="8">8 per page</option>
+            <option value="10">10 per page</option>
+            <option value="20">20 per page</option>
+            <option value="40">40 per page</option>
+          </select>
+        </div>
       </header>
       <div className={mainStyles.general}>
         main - {JSON.stringify(data.slice(0, 20))}
