56e41dabad10ddee1226149757e26502f29724c8
Steven <gonghaima@hotmail.com>
2019-08-19 04:39:54 +1000

    group Pagination component together

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import ReactPaginate from "react-paginate";
-import { Store } from "../store";
-import { fetchDataAction } from "../actions";
-import footerStyles from "../footer.module.css";
-import pagination from "../reducers/pagination";
+import { Store } from "../../store";
+import { fetchDataAction } from "../../actions";
+import paginationStyles from "./pagination.module.css";
 
 const Pagination = ({ history }) => {
   const { state, dispatch } = React.useContext(Store);
   const { apiUrl, currentLimit, currentOffset } = state;
 
   return (
-    <div className={footerStyles.general}>
+    <div className={paginationStyles.general}>
       <ReactPaginate
         previousLabel={"< Previous page"}
         nextLabel={"Next page >"}
