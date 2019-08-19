72bea8f7d0567e1cfb0fa8e97c7c159a2041af18
Steven <gonghaima@hotmail.com>
2019-08-20 02:59:26 +1000

    remove service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import { StoreProvider } from "./store";
-import * as serviceWorker from "./serviceWorker";
 
 const Main = () => (
   <StoreProvider>
@@ -12,8 +11,3 @@ const Main = () => (
 );
 
 render(<Main />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
