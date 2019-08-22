981146b6d442a50e931725f6b1cf9fe71fd2e615
Steven <gonghaima@hotmail.com>
2019-08-23 09:09:51 +1000

    snapshot test for header

---
+++ src/components/Header/header.spec.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./index";
+
+it("Header component renders without crashing", () => {
+  const HeaderComponent = shallow(<Header />);
+  expect(HeaderComponent).toMatchSnapshot();
+});
